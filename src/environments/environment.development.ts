13413f3c02b8deaef5a29d2d89a936308609f030
humbertpa <humbertpal@gmail.com>
2024-09-21 17:51:30 -0600

    control de secretos

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,13 @@
+
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyBC2O_P9mvqcoC59vrfhBq_koCS27dS2gw",
-        authDomain: "proyecto-portafolio-6134b.firebaseapp.com",
-        projectId: "proyecto-portafolio-6134b",
-        storageBucket: "proyecto-portafolio-6134b.appspot.com",
-        messagingSenderId: "615419194799",
-        appId: "1:615419194799:web:ffa7b61dbfd0fb7913d35a",
-        measurementId: "G-400BX0QZ4Z"
+        apiKey: process.env["API_KEY"],
+        authDomain: process.env["AUTH_DOMAIN"],
+        projectId: process.env["PROJECT_ID"],
+        storageBucket: process.env["proyecto-portafolio-6134b.appspot.com"],
+        messagingSenderId: process.env["MESSAGING_SENDER_ID"],
+        appId: process.env["APP_ID"],
+        measurementId: process.env["MEASUREMENT_ID"]
     }
 }
